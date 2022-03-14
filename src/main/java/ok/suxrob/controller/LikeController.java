48481d25a2f87d0d86761b3c2c495f860abf9246
Suxrob <sattorov6606@gmail.com>
2022-03-14 21:24:11 +0500

    Firstdfghjkfghdtjhdhg

--- src/main/java/ok/suxrob/controller/LikeController.java
+++ src/main/java/ok/suxrob/controller/LikeController.java
@@ -1,10 +1,7 @@
 package ok.suxrob.controller;
 
-<<<<<<< HEAD
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-=======
->>>>>>> edf6660ae0d96ffba560949c1fa4ba04cff25f23
 import ok.suxrob.dto.LikeAndDislikeDTO;
 import ok.suxrob.dto.LikeDTO;
 import ok.suxrob.dto.ProfileJwtDTO;
@@ -20,20 +17,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/like")
-<<<<<<< HEAD
 @Api(tags = "Like")
-=======
->>>>>>> edf6660ae0d96ffba560949c1fa4ba04cff25f23
 public class LikeController {
 
     @Autowired
     private LikeService likeService;
 
     @PostMapping
-<<<<<<< HEAD
     @ApiOperation(value = "like create method", notes = "like")
-=======
->>>>>>> edf6660ae0d96ffba560949c1fa4ba04cff25f23
     public ResponseEntity<?> create(@RequestBody LikeDTO dto, HttpServletRequest request) {
         ProfileJwtDTO jwtDTO = JwtUtil.getProfile(request);
         LikeDTO response = likeService.create(dto, jwtDTO.getId());
@@ -41,10 +32,7 @@ public class LikeController {
     }
 
     @GetMapping("/articleId/{id}")
-<<<<<<< HEAD
     @ApiOperation(value = "like articleLikeAndDislikeCount method", notes = "like")
-=======
->>>>>>> edf6660ae0d96ffba560949c1fa4ba04cff25f23
     public ResponseEntity<?> articleLikeAndDislikeCount(@PathVariable("id") Integer articleId,
                                                         @RequestParam("type") LikeType type) {
         LikeAndDislikeDTO count = likeService.articleLikeAndDislikeCount(articleId, type);
@@ -52,10 +40,7 @@ public class LikeController {
     }
 
     @GetMapping("/commentId/{id}")
-<<<<<<< HEAD
     @ApiOperation(value = "like commentLikeAndDislikeCount method", notes = "like")
-=======
->>>>>>> edf6660ae0d96ffba560949c1fa4ba04cff25f23
     public ResponseEntity<?> commentLikeAndDislikeCount(@PathVariable("id") Integer commentId,
                                                         @RequestParam("type") LikeType type) {
         LikeAndDislikeDTO count = likeService.commentLikeAndDislikeCount(commentId, type);
@@ -63,26 +48,15 @@ public class LikeController {
     }
 
     @GetMapping("/action/likedArticle/{id}")
-<<<<<<< HEAD
     @ApiOperation(value = "like likedArticle method", notes = "like")
-=======
->>>>>>> edf6660ae0d96ffba560949c1fa4ba04cff25f23
     public ResponseEntity<?> likedArticle(@PathVariable("id") Integer profileId, HttpServletRequest request) {
         JwtUtil.getProfile(request);
         List<String> articleLikedList = likeService.likedArticle(profileId);
         return ResponseEntity.ok(articleLikedList);
     }
 
-<<<<<<< HEAD
-     public  static void test(){
-         System.out.println("dasdasd");
-     }
-     // git pull origin master --allow-unrelated-histories
     @PutMapping("/action/{id}")
     @ApiOperation(value = "like update method", notes = "like")
-=======
-    @PutMapping("/action/{id}")
->>>>>>> edf6660ae0d96ffba560949c1fa4ba04cff25f23
     public ResponseEntity<?> update(@PathVariable("id") Integer id,
                                     @RequestBody LikeDTO dto, HttpServletRequest request) {
         ProfileJwtDTO jwtDTO = JwtUtil.getProfile(request);
